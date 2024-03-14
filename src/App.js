4dc003458322d6622cb5042d5cabf7136df43255
Nina Wiik <nina@izy.as>
2024-03-14 13:46:41 +0100

    Fixed contact page

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
-        <Route path='/p' element={<Pricing />} />
+        <Route path='/pricing' element={<Pricing />} />
         <Route path='/gallery' element={<Gallery />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,23 +1,62 @@
+import Poppy1 from "../images/Skogen_Hasken_Poppy231014-3-NR-2.jpg-Web.jpg";
+import Poppy2 from "../images/Skogen_Hasken_Poppy231014-85-fullWidth-Web.jpg";
+import Poppy3 from "../images/Skogen_Hasken_Poppy231014-85-Width-Web.jpg";
+import PoppyOpera from "../images/PoppyOpera.jpg";
+import { InstagramEmbed } from 'react-social-media-embed';
+
 export default function Contact() {
   return (
-    <div className='container mx-auto max-w-3xl'>
-    <div className='max-w-3xl mx-6  bg-white p-8 rounded shadow-lg'>
-    <h1 className="text-6xl my-8">CONTACT</h1>
+    <body>
+      <div className="bg-taupe px-4 lg:px-40 flex flex-wrap items-center justify-start">
+        {/* Text column */}
+        <div className="w-full lg:w-2/3 py-8 pr-8">
+          <h2 className="text-cream">Priser og informasjon</h2>
+          <h3 className="mb-6 text-cream">E-mail Me!</h3>
+          <p>
+            Ønsker du å bestille en photoshoot eller har du bare et spørsmål til meg? Da er det bare å sende meg en mail, så kommer jeg tilbake til deg så snart som mulig! Din henvendelse er helt uforpliktende, så ikke nøl med å ta kontakt. Det er best å registrere deg for ønsket dato så tidlig som mulig.
+          </p>
+          <p>
+            Jeg pleier å svare innen noen arbeidsdager. Vær imidlertid oppmerksom på at det avhengig av arbeidsmengden kan ta lengre tid.
+          </p>
+          <div className="mt-6 flex items-center font-bold text-xl">
+            <button className="bg-teal-light hover:bg-teal-dark text-white font-bold py-2 px-4 rounded mt-4">
+              <a href="mailto:hei@ninawiik.no" className="hover:underline font-bold">
+                hei@ninawiik.no
+              </a>
+            </button>
+          </div>
+        </div>
 
-      {/* Mailto link */}
-      <div className='mb-6'>
-          <p className='text-lg font-semibold mb-2'>Er det noe du lurer på eller ønsker å booke en photoshoot?</p>
-          <p className='text-lg font-semibold mb-2'>Send meg en mail, eller hvis det er lettere, så kontakt meg på instagram: {' '}
-              <a href="https://www.instagram.com/ninawiik/" target="_blank" rel="noopener noreferrer">
-                 @ninawiik
-            </a></p>
-          <p className="mt-6">
-        <a href='mailto:hei@ninawiik.no' className='text-blue-500 hover:underline font-bold text-xl'>
-          hei@ninawiik.no
-            </a>
-            </p>
+        {/* Image column */}
+        <div className="w-full lg:w-1/3 p-8">
+          <img src={Poppy1} alt="Bilde" className="rounded-md" />
         </div>
       </div>
+
+      <div className=" bg-blush px-4 lg:px-40 flex flex-wrap items-center justify-start">
+        {/* Text column */}
+        <div className="w-full lg:w-1/2 p-8">
+          <h3 className="mb-6 text-[#21262B]">Follow us on Instagram</h3>
+          <p className="text-[#21262B]">
+            Mange kjenner oss gjennom instagram, så ikke nøl med å sende oss en melding der! Jeg svarer så fort jeg kan for å dele inspirasjon og svare på spørsmål.
+          </p>
+          <p className="text-[#21262B]">
+            På vår Instagram-profil tar jeg deg med på en visuell reise gjennom livet med mine to fantastiske hunder. Fra livlige øyeblikk til "Behind the Scenes" fra våre fotoshoots – alt er med for å inspirere deg.
+          </p>
+          <p className="text-[#21262B]">
+            I tillegg til å dele bilder, gir jeg også bort hemmeligheter fra fotoverdenen og deler tips og triks for hundeelskere. Gjennom stories gir jeg deg et glimt av vår hverdag, slik at du kan bli bedre kjent med oss før du bestemmer deg for å bli med på en magisk fotoshoot-opplevelse!
+          </p>
+        </div>
+        {/* Image column */}
+        <div className="w-full lg:w-1/2 p-8">
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <InstagramEmbed url="https://www.instagram.com/ninawiik/" width={600} />
+          </div>
+        </div>
       </div>
-  )
+      <div className="w-full">
+          <img src={PoppyOpera} alt="Bilde" className="" />
+        </div>
+    </body>
+  );
 }
