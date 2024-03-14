10f686a6e2f0419a0cfec2d73fd0dd269337d6f8
Nina Wiik <nina@izy.as>
2024-03-14 13:48:14 +0100

    Fixed footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,37 +2,44 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  };
+
   return (
-    <footer className="bg-blush text-[#21262B] py-6">
+    <footer className="bg-blush text-[#21262B] font-bold py-6">
       <div className="container mx-auto text-center">
         <ul className="flex justify-center space-x-6">
           <li>
-            <Link to="/" className="text-lg hover:text-teal-dark">
+            <Link to="/" className="text-lg hover:text-teal-dark" onClick={scrollToTop}>
               Home
             </Link>
           </li>
           <li>
-            <Link to="/about" className="text-lg hover:text-teal-dark">
+            <Link to="/about" className="text-lg hover:text-teal-dark" onClick={scrollToTop}>
               About
             </Link>
           </li>
           <li>
-            <Link to="/p" className="text-lg hover:text-teal-dark">
+            <Link to="/pricing" className="text-lg hover:text-teal-dark" onClick={scrollToTop}>
               The Photoshoot
             </Link>
           </li>
           <li>
-            <Link to="/gallery" className="text-lg hover:text-teal-dark">
+            <Link to="/gallery" className="text-lg hover:text-teal-dark" onClick={scrollToTop}>
               Portfolio
             </Link>
           </li>
           <li>
-            <Link to="/contact" className="text-lg hover:text-teal-dark">
+            <Link to="/contact" className="text-lg hover:text-teal-dark" onClick={scrollToTop}>
               Contact
             </Link>
           </li>
         </ul>
-        <p className="text-sm mt-4">© {new Date().getFullYear()} Nina Wiik</p>
+        <p className="text-sm mt-4 text-[#21262B]">© {new Date().getFullYear()} Nina Wiik - Orgnr: 933164268</p>
       </div>
     </footer>
   );
