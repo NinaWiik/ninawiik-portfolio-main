74175f955763a902e8bf89f097f99a8451923e5b
Nina Wiik <nina@izy.as>
2024-03-15 11:53:51 +0100

    Added more pictures to portfolio

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,5 +1,13 @@
 import * as React from "react";
 import ResponsiveGallery from 'react-responsive-gallery';
+import Picture0 from '../gallery/Picture0.jpg';
+import Picture01 from '../gallery/Picture01.jpg';
+import Picture02 from '../gallery/Picture02.jpg';
+import Picture03 from '../gallery/Picture03.jpg';
+import Picture04 from '../gallery/Picture04.jpg';
+import Picture05 from '../gallery/Picture05.jpg';
+import Picture06 from '../gallery/Picture06.jpg';
+import Picture07 from '../gallery/Picture07.jpg';
 import Picture1 from '../gallery/Picture1.jpg';
 import Picture2 from '../gallery/Picture2.jpg';
 import Picture3 from '../gallery/Picture3.jpg';
@@ -8,7 +16,7 @@ import Picture5 from '../gallery/Picture5.jpg';
 import Picture6 from '../gallery/Picture6.jpg';
 import Picture7 from '../gallery/Picture7.jpg';
 import Picture8 from '../gallery/Picture8.jpg';
-// import Picture9 from '../gallery/Picture9.jpg';
+import Picture9 from '../gallery/Picture9.jpg';
 import Picture10 from '../gallery/Picture10.jpg';
 import Picture11 from '../gallery/Picture11.jpg';
 import Picture12 from '../gallery/Picture12.jpg';
@@ -26,15 +34,74 @@ import Picture23 from '../gallery/Picture23.jpg';
 import Picture24 from '../gallery/Picture24.jpg';
 import Picture25 from '../gallery/Picture25.jpg';
 import Picture26 from '../gallery/Picture26.jpg';
+import Picture27 from '../gallery/Picture27.jpg';
+import Picture28 from '../gallery/Picture28.jpg';
+import Picture29 from '../gallery/Picture29.jpg';
 
 
 export default function Gallery() {
     return (
-        <div className="bg-taupe mx-auto mb-10 md:px-24 lg:px-48 xl:px-72">
-            <h1 className="text-6xl mb-8 pt-8">Portfolio</h1>
+      <div className="bg-taupe px-4 lg:px-40 flex flex-wrap items-center justify-start">
+          <h3 className="mt-4 text-cream">Portfolio</h3>
             <ResponsiveGallery
         useLightBox
         images={[
+          {
+            src: Picture0,
+            alt: 'image 1 alt test',
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
+          },
+          {
+            src: Picture01,
+            alt: 'image 1 alt test',
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
+          },
+          {
+            src: Picture02,
+            alt: 'image 1 alt test',
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
+          },
+          {
+            src: Picture03,
+            alt: 'image 1 alt test',
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
+          },
+          {
+            src: Picture04,
+            alt: 'image 1 alt test',
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
+          },
+          {
+            src: Picture05,
+            alt: 'image 1 alt test',
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
+          },
+          {
+            src: Picture06,
+            alt: 'image 1 alt test',
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
+          },
+          {
+            src: Picture07,
+            alt: 'image 1 alt test',
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
+          },
           {
             src: Picture1,
             alt: 'image 1 alt test',
@@ -45,154 +112,172 @@ export default function Gallery() {
           {
             src: Picture2,
             alt: 'image 2 alt test',
-            orderS: 2,
-            orderM: 2,
-            orderL: 2,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture3,
-            orderS: 4,
-            orderM: 3,
-            orderL: 6,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture4,
-            orderS: 3,
-            orderM: 4,
-            orderL: 5,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture5,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture6,
-            orderS: 6,
-            orderM: 5,
-            orderL: 3,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
             // imgClassName: 'img6-style',
           },
           {
             src: Picture7,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture8,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
-          // {
-          //   src: Picture9,
-          //   orderS: 5,
-          //   orderM: 6,
-          //   orderL: 4,
-          // },
+          {
+            src: Picture9,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
+          },
           {
             src: Picture10,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture11,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture12,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture13,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture14,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture15,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture16,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture17,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture18,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture19,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture20,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture21,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture22,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture23,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture24,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture25,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
           {
             src: Picture26,
-            orderS: 5,
-            orderM: 6,
-            orderL: 4,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
+          },
+          {
+            src: Picture27,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
+          },
+          {
+            src: Picture28,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
+          },
+          {
+            src: Picture29,
+            orderS: 1,
+            orderM: 1,
+            orderL: 1,
           },
         ]}
       />
